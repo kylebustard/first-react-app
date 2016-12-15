d83a2fab0fd708463530b536edf0e294b6d58f0e
Kyle Bustard <bustardkyle@gmail.com>
2016-12-15 15:09:41 -0500

    Add state update method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			txt: 'this is the state text',
+			cat: 3
+		}
+	}
+	update( e ){
+		this.setState({txt: e.target.value})
+	}
 	render(){
 		let txt = this.props.txt;
 		let cat = this.props.cat;
 		return (
 			<div>
+			  <input type='text'
+			  onChange={this.update.bind(this)}/>
 			  <h1>{txt}</h1>
-			  <b>The magic number is: {cat}</b>
+			{/* text state is changed, not cat */}
+			  <h2>{this.state.txt} - {this.state.cat}</h2><br/><br/>
+			  <b>The magic number is: {cat}</b><br/><br/>
 			</div>
 		)
 	}
